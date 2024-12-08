0a24ad68d98f3d7a68aa676cfbc1c9dac718a628
Luca Peruzzo <peruzzoluca98@gmail.com>
2024-12-08 15:19:53 +0100

    update project configuration

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import '@angular/compiler';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { bootstrapKcApplication } from './kc.gen';
 import { appConfig } from './app.config';
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types="vitest" />
 
+import { defineConfig } from 'vite';
 import angular from '@analogjs/vite-plugin-angular';
 import { keycloakify } from 'keycloakify/vite-plugin';
-import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
