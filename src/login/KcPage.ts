b406e287f116ebd2fe8ae9c6c840de40bdf7ff72
Stéphane GUILLY <stephane.guilly@s3pweb.com>
2025-03-13 10:03:40 +0100

    add logo and background image

--- src/login/KcPage.ts
+++ src/login/KcPage.ts
@@ -1,6 +1,8 @@
+import "./main.css";
+
 import { getDefaultPageComponent, type KcPage } from '@keycloakify/angular/login';
 import { UserProfileFormFieldsComponent } from '@keycloakify/angular/login/components/user-profile-form-fields';
-import { TemplateComponent } from '@keycloakify/angular/login/template';
+import { TemplateComponent } from './template/template.component';
 import type { ClassKey } from 'keycloakify/login';
 import type { KcContext } from './KcContext';
 
